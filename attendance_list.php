69aa78c55ff10f8346d1c107760df527fa76c811
Mehedi Hasan <46008075+mehedi7253@users.noreply.github.com>
2024-05-06 10:49:19 +0600

    table added

--- attendance_list.php
+++ attendance_list.php
@@ -7,52 +7,49 @@
         </div>
     </div>
     <div class="card-body">
-        <table class="table table-bordered table-hover" id="att-list">
-            <colgroup>
-                <col width="5%">
-                <col width="20%">
-                <col width="20%">
-                <col width="40%">
-            </colgroup>
-            <thead>
-                <tr>
-                    <th class="p-0 text-center">#</th>
-                    <th class="p-0 text-center">Attendance Date</th>
-                    <th class="p-0 text-center">Attendance Time</th>
-                    <th class="p-0 text-center">Employee</th>
-                    <th class="p-0 text-center">Attendance Type</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                $today = @date("Y-m-d");
-                $att_qry = $conn->query("SELECT a.*,t.name as tname,(e.lastname || ', ' || e.firstname || ' ' || e.middlename) as `fullname`,e.employee_code FROM `attendance_list` a inner join employee_list e on a.employee_id = e.employee_id inner join att_type_list t on a.att_type_id = t.att_type_id");
-                //  $att_qry = $conn->query("SELECT a.*,t.name as tname,(e.lastname || ', ' || e.firstname || ' ' || e.middlename) as `fullname`,e.employee_code FROM `attendance_list` a inner join employee_list e on a.employee_id = e.employee_id inner join att_type_list t on a.att_type_id = t.att_type_id where date(a.date_created) = '".date("Y-m-d")."' ");
-                 $i = 1;
-                 while($row = $att_qry->fetchArray()):
-                    // echo $row['fullname'];
-                    $bg = "primary";
-                    if(in_array($row['att_type_id'],array(2,4)))
-                    $bg = "danger";
-                ?>
-                <tr>
-                    <td class="align-middle py-0 px-1 text-center"><?php echo $i++; ?></td>
-                    <td class="align-middle py-0 px-1 text-center"><?php echo date("Y M d",strtotime($row['date_created']))  ?></td>
-                    <td class="align-middle py-0 px-1 text-center"><?php echo date("h:i A",strtotime($row['date_created']))  ?></td>
-                    <td class="align-middle py-0 px-1">
-                        <p class="m-0">
-                            <small><b>Employee Code:</b> <?php echo $row['employee_code'] ?></small><br>
-                            <small><b>Name:</b> <?php echo $row['fullname'] ?></small>
-                        </p>
-                    </td>
-                    <td class="align-middle py-0 px-1 text-center">
-                        <span class="badge bg-<?php echo $bg ?>"><?php echo $row['tname'] ?></span>
-                    </td>
-                   
-                </tr>
-                <?php endwhile; ?>
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-striped" id="example">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Attendance Date</th>
+                        <th>Attendance Time</th>
+                        <th>Employee</th>
+                        <th>Attendance Type</th>
+                    </tr>
+                </thead>
+                    <?php 
+                        $today = @date("Y-m-d");
+                        $att_qry = $conn->query("SELECT a.*,t.name as tname,(e.lastname || ', ' || e.firstname || ' ' || e.middlename) as `fullname`,e.employee_code FROM `attendance_list` a inner join employee_list e on a.employee_id = e.employee_id inner join att_type_list t on a.att_type_id = t.att_type_id");
+                        //  $att_qry = $conn->query("SELECT a.*,t.name as tname,(e.lastname || ', ' || e.firstname || ' ' || e.middlename) as `fullname`,e.employee_code FROM `attendance_list` a inner join employee_list e on a.employee_id = e.employee_id inner join att_type_list t on a.att_type_id = t.att_type_id where date(a.date_created) = '".date("Y-m-d")."' ");
+                        // $i = 1;
+                        // while($row = $att_qry->fetchArray()):
+                        //     // echo $row['fullname'];
+                        //     $bg = "primary";
+                        //     if(in_array($row['att_type_id'],array(2,4)))
+                        //     $bg = "danger";
+                    ?>
+                <tbody>
+                    <?php $i = 1;
+                    while($row = $att_qry->fetchArray()) {?>
+                    <tr>
+                        <td class="align-middle py-0 px-1 text-center"><?php echo $i++; ?></td>
+                        <td class="align-middle py-0 px-1 text-center"><?php echo date("Y M d",strtotime($row['date_created']))  ?></td>
+                        <td class="align-middle py-0 px-1 text-center"><?php echo date("h:i A",strtotime($row['date_created']))  ?></td>
+                        <td class="align-middle py-0 px-1">
+                            <p class="m-0">
+                                <small><b>Employee Code:</b> <?php echo $row['employee_code'] ?></small><br>
+                                <small><b>Name:</b> <?php echo $row['fullname'] ?></small>
+                            </p>
+                        </td>
+                        <td class="align-middle py-0 px-1 text-center">
+                            <span class="badge bg-<?php echo $bg ?>"><?php echo $row['tname'] ?></span>
+                        </td>
+                    </tr>
+                    <?php }?>
+                </tbody>
+            </table>
+        </div> 
     </div>
 </div>
 <script>
